70ce0e8437342213640206d5d26ffab4b08cc417
Camila CROSET <potycamila@gmail.com>
2025-04-14 15:01:00 +0200

    Initial layout implementation completed and polished Home page visuals

--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -1,6 +1,26 @@
+import './style.scss'
+
 function SignIn() {
     return (
-        <div><h1>hi</h1></div>
+        <main className="sign-in">
+            <div className="sign-in__content">
+                <i className="fa fa-user-circle sign-in-icon"></i> 
+                <h1>Sign In</h1>
+                <div className="input-wrapper">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" />
+                </div>
+                <div className="input-wrapper">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" />
+                </div>
+                <div className="input-remember">
+                    <input type="checkbox" id="remember-me" /><label htmlFor="remember-me">Remember me</label>
+                </div>
+                <button className='sign-in__button'><a href='./user'></a>Sign In</button>
+            </div>
+            
+        </main>
     )
 }
 
